48c27871a2e1e07a36f7de6077b3bd75b285ed03
Little-Mage <lm4ssw@gmail.com>
2024-10-02 14:06:08 -0600

    Update premium.js

--- premium.js
+++ premium.js
@@ -5,7 +5,7 @@ let coin = parseFloat(document.getElementById('c').value);
 
 let melt = silver * troy;
 let premium = (coin - melt).toFixed(2);
-let percent = ((coin - melt) / melt * 100).toFixed(1);
+let percent = (((coin - melt) / melt) * 100).toFixed(1);
 
 document.getElementById('p1').innerHTML = 'Premium: $' + premium;
 document.getElementById('p2').innerHTML = 'Percent: ' + percent + '%';
